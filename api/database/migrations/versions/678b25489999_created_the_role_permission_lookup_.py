02d1a2fd11703c63853416364070193e6f21b4c1
OMTut <60162459+OMTut@users.noreply.github.com>
2025-08-05 09:37:16 -0500

    created migration for role_permission table

---
+++ api/database/migrations/versions/678b25489999_created_the_role_permission_lookup_.py
@@ -0,0 +1,32 @@
+"""createed the role_permission lookup table
+
+Revision ID: 678b25489999
+Revises: 087f8ddfe93e
+Create Date: 2025-08-05 08:41:12.304349
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '678b25489999'
+down_revision: Union[str, None] = '087f8ddfe93e'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('role_permission',
+        sa.Column('role_id', sa.Integer(), nullable=False),
+        sa.Column('permission_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['role_id'], ['roles.role_id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['permission_id'], ['permissions.permission_id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('role_id', 'permission_id')
+        )
+
+
+def downgrade() -> None:
+    op.drop_table('role_permission')
